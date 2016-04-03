7789a6d0c82f523e20b65640d3d2d5a737fdd9f9
rfblue2 <rfblue2@gmail.com>
2016-04-03 06:42:10 -0400

    Tiny bug fix

--- client/home.js
+++ client/home.js
@@ -1,6 +1,11 @@
+// Initialize faces_arr with dummy data
+var faces_arr: [{_id:"http://www.princeton.edu/deptafe_internal/cimg!0/dxl1kmtla6gcraqorukj95nrmavdsts", name:"Roland"}, 
+                {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/axtz0nij9jr2qctwcqeq2dauwplreqx", name:"Casey"}, 
+                {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/iuilrhps9y0anb86plua7bbrh7d7jhe", name:"Matthew"}, 
+                {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/lsgnytv9w92nzlmq67rhu7y1wv92bj", name:"Evan"}, 
+                {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn", name:"Gordon"}];
 
-
-Template.home.created() = function() {
+Template.home.created = function() {
 	face_arr = Session.get("getInitialFaces");
 	Session.set("face_arr", face_arr);
 };
