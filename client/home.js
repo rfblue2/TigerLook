c3050388db0387b222ff5676e6a85fe88537350d
rfblue2 <rfblue2@gmail.com>
2016-04-03 06:29:25 -0400

    Added client code calling database funct

--- client/home.js
+++ client/home.js
@@ -1,14 +1,8 @@
-face_arr = [{_id:"http://www.princeton.edu/deptafe_internal/cimg!0/dxl1kmtla6gcraqorukj95nrmavdsts", name:"Roland"}, 
-            {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/axtz0nij9jr2qctwcqeq2dauwplreqx", name:"Casey"}, 
-            {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/iuilrhps9y0anb86plua7bbrh7d7jhe", name:"Matthew"}, 
-            {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/lsgnytv9w92nzlmq67rhu7y1wv92bj", name:"Evan"}, 
-            {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn", name:"Gordon"}];
 
-Session.set("face_arr", face_arr);
 
 Template.home.created() = function() {
-	// initial faces
-	console.log("created");
+	face_arr = Session.get("getInitialFaces");
+	Session.set("face_arr", face_arr);
 };
 
 Template.home.helpers({
@@ -20,28 +14,23 @@ Template.home.helpers({
 Template.home.events({
 	'click button'(event, instance) {
 		console.log("clicked button");
-		// reset
-		// get initial set of faces
+		Session.get("reset");
+		face_arr = Session.get("getInitialFaces");
+		Session.set("face_arr", face_arr);
 	}
 });
 
 Template.face.events({
 	'click .clickable_face'(event, instance) {
 		console.log(Blaze.getData(event.target)._id);
-		var testId = Blaze.getData(event.target)._id;
-		for (var i = 0; i < face_arr.length; i++) {
-			if (testId == face_arr[i]._id) {
-				$( ".clickable_face" ).animate({'opacity' : 0}, function() {
-					// get next faces and set them to face_arr
-					face_arr[i] = {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn", name:"Gordon"};
-					Session.set("face_arr", face_arr);
+		var clickedId = Blaze.getData(event.target)._id;
+		$( ".clickable_face" ).animate({'opacity' : 0}, function() {
+			// get next faces and set them to face_arr
+			face_arr = (Session.get("getNextFaces"))(clickedId);
+			Session.set("face_arr", face_arr);
 
-					$( ".clickable_face" ).animate({'opacity' : 1});
-				});
-				
-				break;
-			}
-		}
+			$( ".clickable_face" ).animate({'opacity' : 1});
+		});
 
 	}
 });
