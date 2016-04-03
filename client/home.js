20cdb862bbb77122602c048f5a292f109b0b1d07
rfblue2 <rfblue2@gmail.com>
2016-04-03 04:57:26 -0400

    Little name changes

--- client/home.js
+++ client/home.js
@@ -1,8 +1,8 @@
-face_arr = [{id:"1", lnk:"http://www.princeton.edu/deptafe_internal/cimg!0/dxl1kmtla6gcraqorukj95nrmavdsts", name:"Roland"}, 
-            {id:"2", lnk:"http://www.princeton.edu/deptafe_internal/cimg!0/axtz0nij9jr2qctwcqeq2dauwplreqx", name:"Casey"}, 
-            {id:"3", lnk:"http://www.princeton.edu/deptafe_internal/cimg!0/iuilrhps9y0anb86plua7bbrh7d7jhe", name:"Matthew"}, 
-            {id:"4", lnk:"http://www.princeton.edu/deptafe_internal/cimg!0/lsgnytv9w92nzlmq67rhu7y1wv92bj", name:"Evan"}, 
-            {id:"5", lnk:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn", name:"Gordon"}];
+face_arr = [{_id:"http://www.princeton.edu/deptafe_internal/cimg!0/dxl1kmtla6gcraqorukj95nrmavdsts", name:"Roland"}, 
+            {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/axtz0nij9jr2qctwcqeq2dauwplreqx", name:"Casey"}, 
+            {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/iuilrhps9y0anb86plua7bbrh7d7jhe", name:"Matthew"}, 
+            {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/lsgnytv9w92nzlmq67rhu7y1wv92bj", name:"Evan"}, 
+            {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn", name:"Gordon"}];
 
 Session.set("face_arr", face_arr);
 
@@ -12,7 +12,7 @@ Template.home.helpers({
 
 Template.face.events({
 	'click .clickable_face'(event, instance) {
-		console.log(Blaze.getData(event.target).id); // If you click a face you get it's id in an alert
+		console.log(Blaze.getData(event.target).id);
 		var testId = Blaze.getData(event.target).id;
 		for (var i = 0; i < face_arr.length; i++) {
 			if (testId == face_arr[i].id) {
