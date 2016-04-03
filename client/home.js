45afabdd0abd831ee1ce6d4a0cf30cadc0fcb545
rfblue2 <rfblue2@gmail.com>
2016-04-03 05:28:57 -0400

    Fading animation

--- client/home.js
+++ client/home.js
@@ -7,18 +7,23 @@ face_arr = [{_id:"http://www.princeton.edu/deptafe_internal/cimg!0/dxl1kmtla6gcr
 Session.set("face_arr", face_arr);
 
 Template.home.helpers({
-    faces: function () { return Session.get("face_arr"); }
+    faces: function () { 
+				return Session.get("face_arr");
+			}
 });
 
 Template.face.events({
 	'click .clickable_face'(event, instance) {
-		console.log(Blaze.getData(event.target).id);
-		var testId = Blaze.getData(event.target).id;
+		console.log(Blaze.getData(event.target)._id);
+		var testId = Blaze.getData(event.target)._id;
 		for (var i = 0; i < face_arr.length; i++) {
-			if (testId == face_arr[i].id) {
-				console.log("in loop");
-				face_arr[i] = {id:"5", lnk:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn", name:"Gordon"};
-				Session.set("face_arr", face_arr);
+			if (testId == face_arr[i]._id) {
+				$( ".clickable_face" ).animate({'opacity' : 0}, function() {
+					face_arr[i] = {_id:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn", name:"Gordon"};
+					Session.set("face_arr", face_arr);
+					$( ".clickable_face" ).animate({'opacity' : 1});
+				});
+				
 				break;
 			}
 		}
