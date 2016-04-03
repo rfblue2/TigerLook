22d329819367a41cc150c37f33889b414c8f4f71
Roland F <rfblue2@gmail.com>
2016-04-03 00:00:45 -0400

    Display faces

--- TigerLook.js
+++ TigerLook.js
@@ -8,7 +8,12 @@ if (Meteor.isClient) {
                 {src:"http://www.princeton.edu/deptafe_internal/cimg!0/axtz0nij9jr2qctwcqeq2dauwplreqx"}, 
                 {src:"http://www.princeton.edu/deptafe_internal/cimg!0/iuilrhps9y0anb86plua7bbrh7d7jhe"}, 
                 {src:"http://www.princeton.edu/deptafe_internal/cimg!0/lsgnytv9w92nzlmq67rhu7y1wv92bj"}, 
-                {src:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn"}]
+                {src:"http://www.princeton.edu/deptafe_internal/cimg!0/ogcys7dc8co5b9ztrhjv4h663yvgsgn"}],
+        names: [{name:"Roland"},
+                {name:"Casey"},
+                {name:"Matthew"},
+                {name:"Evan"},
+                {name:"Gordon"}]
     });
 }
 
