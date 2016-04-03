648ba107b0e6e20c6944cc6357cf4de41dfe4eca
Matthew Li <matthewli1997@gmail.com>
2016-04-02 23:53:57 -0400

    changed to just 3 eigenfaces

--- efaces/pairwisecompare.sh
+++ efaces/pairwisecompare.sh
@@ -4,9 +4,9 @@ cd ..
 for f in *.png
 do
     echo "https://www.princeton.edu/deptafe_internal/cimg%210/$f" 1>> efaces/output.txt
-    br -algorithm FaceRecognition -compare efaces/e1.png $f 1>> efaces/output.txt 2>> /dev/null
+    # br -algorithm FaceRecognition -compare efaces/e1.png $f 1>> efaces/output.txt 2>> /dev/null
     br -algorithm FaceRecognition -compare efaces/e2.png $f 1>> efaces/output.txt 2>> /dev/null
     br -algorithm FaceRecognition -compare efaces/e3.png $f 1>> efaces/output.txt 2>> /dev/null
     br -algorithm FaceRecognition -compare efaces/e4.png $f 1>> efaces/output.txt 2>> /dev/null
-    br -algorithm FaceRecognition -compare efaces/e5.png $f 1>> efaces/output.txt 2>> /dev/null
+    # br -algorithm FaceRecognition -compare efaces/e5.png $f 1>> efaces/output.txt 2>> /dev/null
 done
